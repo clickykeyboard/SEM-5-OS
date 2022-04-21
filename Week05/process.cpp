bbf3bc4d475599012d1efdf963943773af992cff
clickykeyboard <68023752+clickykeyboard@users.noreply.github.com>
2022-04-21 23:19:04 +0500

    Update process.cpp

--- Week05/process.cpp
+++ Week05/process.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main() {
  int process = fork();
  if (process == 0) {
-  cout << "Parent process" << endl;
- } else {
   cout << "Child process" << endl;
+ } else {
+  cout << "Parent process" << endl;
  }
 }

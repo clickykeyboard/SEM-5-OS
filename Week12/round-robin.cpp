aa2845b5c41aca442c0d8dd92e869afb0d61b0cf
clickykeyboard <68023752+clickykeyboard@users.noreply.github.com>
2022-06-07 20:35:13 +0500

    formatting

--- Week12/round-robin.cpp
+++ Week12/round-robin.cpp
@@ -11,91 +11,96 @@ int processes;
 int processJustExecuted = -1;
 
 void determineReadyQueue(int *arrivalTime, int *burstTime) {
-    for (int i = 0; i < processes; i++) {
-        if (i == processJustExecuted || burstTime[i] < 0)
-            continue;
-        
-        if (arrivalTime[i] <= currentCPUTime) {
-            // Check if process does not already exist in ready queue
-            if (find(readyQueue.begin(), readyQueue.end(), i) == readyQueue.end())
-                readyQueue.push_back(i);
-       
-        }
-    }
+  for (int i = 0; i < processes; i++) {
+    if (i == processJustExecuted || burstTime[i] < 0)
+      continue;
     
-    // Add the process that was just executed to the end of queue
-    if (processJustExecuted != -1 && burstTime[processJustExecuted] != 9999)
-        readyQueue.push_back(processJustExecuted);
+    if (arrivalTime[i] <= currentCPUTime) {
+      // Check if process does not already exist in ready queue
+      if (find(readyQueue.begin(), readyQueue.end(), i) == readyQueue.end())
+        readyQueue.push_back(i);
+    
+    }
+  }
+  
+  // Add the process that was just executed to the end of queue
+  if (processJustExecuted != -1 && burstTime[processJustExecuted] != 9999)
+    readyQueue.push_back(processJustExecuted);
 }
 
 int main() {
-    cout << "Enter the number of processes: ";
-    cin >> processes;
+  cout << "Enter the number of processes: ";
+  cin >> processes;
+  
+  int timeQuantum;
+  cout << "Enter time quantum: ";
+  cin >> timeQuantum;
+  
+  int totalArrivalTime[processes],
+      remainingArrivalTime[processes],
+      totalBurstTime[processes],
+      remainingBurstTime[processes],
+      completionTime[processes];
+  
+  for (int i = 0; i < processes; i++) {
+    cout << "Enter arrival time for process " << i << ": ";
+    cin >> totalArrivalTime[i];
+    remainingArrivalTime[i] = totalArrivalTime[i];
     
-    int timeQuantum;
-    cout << "Enter time quantum: ";
-    cin >> timeQuantum;
+    cout << "Enter burst time for process " << i << ": ";
+    cin >> totalBurstTime[i];
+    remainingBurstTime[i] = totalBurstTime[i];
+  }
+
+  /* Round Robin Scheduling */
+  int completedProcesses = 0;
+  int modifiedTimeQuantum;
+  while (completedProcesses != processes) {
+  	modifiedTimeQuantum = timeQuantum;
+  	
+    determineReadyQueue(remainingArrivalTime, remainingBurstTime);
     
-    int totalArrivalTime[processes],
-        remainingArrivalTime[processes],
-        totalBurstTime[processes],
-        remainingBurstTime[processes],
-        completionTime[processes];
+    processJustExecuted = readyQueue.front();
+    readyQueue.pop_front();
     
-	for (int i = 0; i < processes; i++) {
-		cout << "Enter arrival time for process " << i << ": ";
-		cin >> totalArrivalTime[i];
-		remainingArrivalTime[i] = totalArrivalTime[i];
-		
-		cout << "Enter burst time for process " << i << ": ";
-		cin >> totalBurstTime[i];
-		remainingBurstTime[i] = totalBurstTime[i];
-	}
-	
-	/* Round Robin Scheduling */
-	int completedProcesses = 0;
-	int modifiedTimeQuantum;
-	while (completedProcesses != processes) {
-	    modifiedTimeQuantum = timeQuantum;
-	
-	    determineReadyQueue(remainingArrivalTime, remainingBurstTime);
-	    
-	    processJustExecuted = readyQueue.front();
-	    readyQueue.pop_front();
-		
-	    if (remainingBurstTime[processJustExecuted] < timeQuantum)
-	        modifiedTimeQuantum = remainingBurstTime[processJustExecuted];
-		
-	    remainingBurstTime[processJustExecuted] -= modifiedTimeQuantum;
-	    
-	    /* Gantt chart */
-		cout << "|P" << processJustExecuted << "|";
-		
-	    currentCPUTime += timeQuantum;
-	    if (remainingBurstTime[processJustExecuted] <= 0) {
-	        completionTime[processJustExecuted] = currentCPUTime + 1;
-	        
-	        remainingBurstTime[processJustExecuted] = 9999;
-            	remainingArrivalTime[processJustExecuted] = 9999;
-            
-            	completedProcesses++;
-	    }
-	}
-	
-	int turnaroundTime[processes] = {0}; /* Turnaround time (CT - AT) */
-    int waitingTime[processes] = {0}; /* Waiting time (TAT - BT) */
-    for (int i = 0; i < processes; i++) {
-        turnaroundTime[i] = completionTime[i] - totalArrivalTime[i];
-        waitingTime[i] = turnaroundTime[i] - totalBurstTime[i];
+    if (remainingBurstTime[processJustExecuted] < timeQuantum)
+    	modifiedTimeQuantum = remainingBurstTime[processJustExecuted];
+    
+    remainingBurstTime[processJustExecuted] -= modifiedTimeQuantum;
+    
+    /* Gantt chart */
+    cout << " | " << currentCPUTime << " | ";
+    cout << "|P" << processJustExecuted + 1<< "|";
+
+    currentCPUTime += modifiedTimeQuantum;
+    if (remainingBurstTime[processJustExecuted] <= 0) {
+    
+      completionTime[processJustExecuted] = currentCPUTime;
+        
+      remainingBurstTime[processJustExecuted] = 9999;
+      remainingArrivalTime[processJustExecuted] = 9999;
+          
+      completedProcesses++;
     }
     
-    cout << endl << "P\t" << "AT\t" << "BT\t" << "CT\t" << "TAT\t" << "WT" << endl;
-    for (int i = 0; i < processes; i++) {
-        cout << "P" << i << "\t"
-            << totalArrivalTime[i] << "\t"
-            << totalBurstTime[i] << "\t"
-            << completionTime[i] << "\t"
-            << turnaroundTime[i] << "\t"
-            << waitingTime[i] << endl;
-    }
+	}
+	
+	cout << " | " << currentCPUTime << " | ";
+
+  int turnaroundTime[processes] = {0}; /* Turnaround time (CT - AT) */
+  int waitingTime[processes] = {0}; /* Waiting time (TAT - BT) */
+  for (int i = 0; i < processes; i++) {
+    turnaroundTime[i] = completionTime[i] - totalArrivalTime[i];
+    waitingTime[i] = turnaroundTime[i] - totalBurstTime[i];
+  }
+  
+  cout << endl << "P\t" << "AT\t" << "BT\t" << "CT\t" << "TAT\t" << "WT" << endl;
+  for (int i = 0; i < processes; i++) {
+    cout << "P" << i + 1 << "\t"
+          << totalArrivalTime[i] << "\t"
+          << totalBurstTime[i] << "\t"
+          << completionTime[i] << "\t"
+          << turnaroundTime[i] << "\t"
+          << waitingTime[i] << endl;
+  }
 }
